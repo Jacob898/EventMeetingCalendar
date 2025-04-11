6109902c8887e40149c7c0f87845405dc3165bea
Jacob898 <jstach898@gmail.com>
2025-04-11 14:19:56 +0200

    Added spring project

---
+++ EventMeetingCalendarBackend/src/main/java/org/spring/eventcalendar/eventmeetingcalendarbackend/EventMeetingCalendarBackendApplication.java
@@ -0,0 +1,13 @@
+package org.spring.eventcalendar.eventmeetingcalendarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventMeetingCalendarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventMeetingCalendarBackendApplication.class, args);
+    }
+
+}
---
+++ EventMeetingCalendarBackend/src/test/java/org/spring/eventcalendar/eventmeetingcalendarbackend/EventMeetingCalendarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.eventcalendar.eventmeetingcalendarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventMeetingCalendarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
